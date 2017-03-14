8ca1f3a18d8f2393f833a618bbc5bcf7d7a5051c
Chris Marcenelle <chrismarcenelle@Chriss-MBP.home>
2017-03-14 10:01:03 -0400

    created if statements

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,13 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert ('You found a match!');
+}
+else if (cardOne === cardThree) {
+	alert ('You found a match!');
+}
+else {
+	alert ('Sorry, try again');
+}
